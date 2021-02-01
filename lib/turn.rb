74b46057b932a66b5971ed60a222fec5b99ec20e
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-01 23:29:49 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -45,9 +45,10 @@ def turn(board)
   #capture the users move
   input = gets.strip
   index = input_to_index(input)
-  until valid_move?(board, index) == true
+  if condition
+    move(board, index, value="X")
+    display_board(board)
+  else
     turn(board)
   end
-  move(board, index, value="X")
-  display_board(board)
 end
